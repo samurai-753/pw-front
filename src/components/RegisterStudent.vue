8077f50647bb2c9f7b2bc57e62321c2b34de26f6
joaopedroteo <jpteodoro2010@gmail.com>
2019-05-14 14:22:06 -0300

    botoes na tela de cadastro

--- src/components/RegisterStudent.vue
+++ src/components/RegisterStudent.vue
@@ -1,21 +1,29 @@
 <template>
   <div class='main-container'>
-    <v-container grid-list-xl>
-      <v-layout v-if="showList">
-        <v-flex s12>
-          <h2 class="table-title">Cadastrar Aluno</h2>
-          <v-flex xs12 sm6 md4>
-            <v-form ref="form">
-              <v-text-field label="Nome do Aluno" required></v-text-field>
-              <v-text-field label="Email institucional" required></v-text-field>
-              <v-text-field label="Curso" required></v-text-field>
-              <v-select :items="categorias" label="Categoria" required></v-select>
-              <v-select :items="projetos" label="Projeto"></v-select>
-            </v-form>
+    <v-flex>
+      <h2 class="table-title">Cadastrar Aluno</h2>
+    </v-flex>
+    <v-form ref="form">
+      <v-container grid-list-xl>
+        <v-layout v-if="showList" >
+          <v-flex xs12>
+                <v-text-field label="Nome do Aluno" required></v-text-field>
+                <v-text-field label="Curso" required></v-text-field>
+                <v-select :items="projetos" label="Projeto"></v-select>
           </v-flex>
-        </v-flex>
-      </v-layout>
-    </v-container>
+          <v-flex xs12>
+                <v-text-field label="Email institucional" required></v-text-field>
+                <v-select :items="categorias" label="Categoria" required></v-select>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-form>
+    <v-flex xs12>
+    <v-layout>
+      <v-btn color="success">Cadastrar</v-btn>
+      <v-btn color="error">Cancelar</v-btn>
+    </v-layout>
+    </v-flex>
   </div>
 </template>
 
@@ -32,7 +40,7 @@ export default {
       title: 'Cadastrar Aluno',
       showList: true,
       right: null,
-      categorias: ['Iniciação científica', 'Mestrado', 'Doutorade', 'Pós-doutorado'],
+      categorias: ['Iniciação científica', 'Mestrado', 'Doutorado', 'Pós-doutorado'],
       projetos: []
     }
   },
