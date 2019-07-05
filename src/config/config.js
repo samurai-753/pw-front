4afea0e8e94b94bcde3cb007f80367c1f49d2758
Eduardo Lima <eduardo.fernandodelima@gmail.com>
2019-07-04 23:04:58 -0300

    corrige rota

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://enigmatic-anchorage-17249.herokuapp.com/api"
+export const API_URL = "https://enigmatic-anchorage-17249.herokuapp.com/api"
 
 // export const API_URL = "http://192.168.0.120:58913/api"
